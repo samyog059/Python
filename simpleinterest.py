3971d43e363c6c02980a09c3a27e56d60661f027
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-24 16:24:27 +0545

    New updation

---
+++ simpleinterest.py
@@ -0,0 +1,5 @@
+Principle = int(input("Enter Principle amount:"))
+Rate = int(input("Enter Rate:"))
+Time = int(input("Enter time period:"))
+Simple_Interest = (Principle * Time * Rate) / 100
+print("Simple Interest:", Simple_Interest)
